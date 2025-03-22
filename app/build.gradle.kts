4b153987dc671526a3352e48d0a59d4cee94866c
baset <baset.esmaili0@gmail.com>
2025-03-22 22:28:37 +0330

    Update Gradle and library versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,5 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     implementation(projects.reactiveLocationLocationmanager)
 }
